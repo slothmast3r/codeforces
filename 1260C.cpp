57d2964db3c6dfa9373736b6173278fe7c322ca6
slothmast3r <35583911+slothmast3r@users.noreply.github.com>
2019-12-13 20:49:01 +0100

    Add files via upload

---
+++ 1260C.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int q;
+	cin >> q;
+	while (q--) {
+		long long a, b, g, k;
+		cin >> a >> b >> k;
+		g = __gcd(a, b);
+		a /= g;
+		b /= g;
+		if (a > b) swap(a, b);
+		if ((k - 1) * a + 1 < b) cout << "REBEL";
+		else cout << "OBEY";
+		cout << endl;
+	}
+	return 0;
+}
